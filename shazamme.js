0f75cb47e7a0042d352e4e15b598ee668dc8cda6
Justin McCullough <justin@cretin.co>
2023-09-14 13:44:09 -0700

    Added a Google API wrapper.

--- shazamme.js
+++ shazamme.js
@@ -759,6 +759,52 @@
             }
         }
 
+        this.gapi = (k, v = 'beta') => {
+            const maps = (l = [], _v = v) => {
+                ( g=> {
+                    var h,
+                        a,
+                        k,
+                        p = "The Google Maps JavaScript API",
+                        c = "google",
+                        l = "importLibrary",
+                        q = "__ib__",
+                        m = document,
+                        b = window;
+
+                    b= b[c] || (b[c]={});
+
+                    var d = b.maps || (b.maps={}),
+                        r = new Set,
+                        e = new URLSearchParams,
+                        u= () => h || (h=new Promise( async(f,n) => {
+                            await (a = m.createElement("script"));
+
+                            e.set("libraries", [...r] + "");
+
+                            for (k in g) e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);
+                                e.set("callback",c+".maps."+q);
+
+                            a.src=`https://maps.${c}apis.com/maps/api/js?`+e;
+                            d[q] = f;
+
+                            a.onerror = () => h = n(Error(p+" could not load."));
+                            a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+                            m.head.append(a)
+                        }));
+
+                    d[l] ? console.warn(p+" only loads once. Ignoring:",g)
+                        : d[l] = (f,...n) => r.add(f) && u().then( () => d[l](f,...n))
+                }) ( {key: k, v: v,} );
+
+                return Promise.all(l?.filter( i => i?.length > 0 ).map( i => google.maps.importLibrary(i) ) || []);
+            }
+
+            return {
+                maps: maps
+            }
+        }
+
         this.currentUser = (refresh = false) =>
             (this._session && !refresh && Promise.resolve({...this._session}))
             || (localStorage._s && sender.auth(JSON.parse(atob(localStorage._s)).email))

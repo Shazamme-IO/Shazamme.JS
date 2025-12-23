4945ab3c1951d6078c999988f9498c3d2e3e4ef2
Justin McCullough <justin@cretin.co>
2025-12-23 14:54:43 -0700

    Updated server references for staging requests.

--- shazamme.js
+++ shazamme.js
@@ -515,7 +515,7 @@
                         }
 
                         $.ajax({
-                            url: 'https://staging.shazamme.salsa.hosting/Job-Listing/src/php/actions',
+                            url: 'https://staging.shazamme.io/Job-Listing/src/php/actions',
                             type: 'POST',
                             data: JSON.stringify({
                                 action: 'Get Site ID',
@@ -523,9 +523,9 @@
                             })
                         }).then( res => {
                             sender._site = (res.status && res.response.items.length > 0 && res.response.items[0]) || {};
-                            sender._site.ActionUrl = 'https://staging.shazamme.salsa.hosting/Job-Listing/src/php/actions';
-                            sender._site.RegionalUrl = 'https://staging.shazamme.salsa.hosting/Job-Listing/src/php/regional/actions';
-                            sender._site.documentUri = 'https://staging.shazamme.salsa.hosting/candidate-document/';
+                            sender._site.ActionUrl = 'https://staging.shazamme.io/Job-Listing/src/php/actions';
+                            sender._site.RegionalUrl = 'https://staging.shazamme.io/Job-Listing/src/php/regional/actions';
+                            sender._site.documentUri = 'https://staging.shazamme.io/candidate-document/';
 
                             resolve(sender._site);
                         });
@@ -996,7 +996,7 @@
             sender.site().then( s => {
                 const uri = s?.isLive
                     ? 'https://shazamme.io/Job-Listing/src/php/client/actions'
-                    : 'https://staging.shazamme.salsa.hosting/Job-Listing/src/php/client/actions';
+                    : 'https://staging.shazamme.io/Job-Listing/src/php/client/actions';
 
                 return Promise.resolve({
                     submit: (d) =>
@@ -1089,7 +1089,7 @@
         this.script = (src) =>
             new Promise( (res, rej) => {
                 $.getScript(src)
-                    .done( () => res() )
+                    .done( () => { res(); })
                     .fail( () => { console.warn('WARNING: Resource unavailable', src); res(); });
             });
 
